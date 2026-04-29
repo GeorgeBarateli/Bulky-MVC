879698d628156254048dfb2dd0eee9f968014d66
Givi Barateli <giobarateli@gmail.com>
2026-04-29 08:51:35 -0500

    added category view and controller

---
+++ BulkyWeb/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
